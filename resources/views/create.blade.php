81fd6e5caa44ce26ef4690d9d082c2f108d7d0a7
Pawel Kawa <ramzord@gmail.com>
2025-01-22 22:08:24 +0000

    Created first component, form

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -10,29 +10,5 @@
 	}
 	</style>
 @section('content')
-	<form action="{{ route('tasks.store') }}" method="post">
-		@csrf
-		<div class="form-group">
-			<label for="title">Title</label>
-			<input type="text" name="title" id="title" value="{{old('title')}}" class="form-control">
-			@error('title')
-				<div class="error-message">{{ $message }}</div>
-			@enderror
-		</div>
-		<div class="form-group">
-			<label for="description">Description</label>
-			<textarea name="description" id="description" value="{{old('description')}}" class="form-control"></textarea>
-			@error('description')
-				<div class="error-message">{{ $message }}</div>
-			@enderror
-		</div>
-		<div class="form-group">
-			<label for="long_description">Long description</label>
-			<textarea name="long_description" id="long_description" value="{{old('long_description')}}" class="form-control"></textarea>
-			@error('long_description')
-				<div class="error-message">{{ $message }}</div>
-			@enderror
-		</div>
-		<button type="submit">Add task</button>
-	</form>
+  @include('component.form')
 @endsection
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -10,30 +10,5 @@
 	}
 	</style>
 @section('content')
-	<form action="{{ route('tasks.update', ['task' => $task->id]) }}" method="post">
-		@csrf
-		@method('put')
-		<div class="form-group">
-			<label for="title">Title</label>
-			<input type="text" name="title" value="{{$task->title}}" id="title" class="form-control">
-			@error('title')
-				<div class="error-message">{{ $message }}</div>
-			@enderror
-		</div>
-		<div class="form-group">
-			<label for="description">Description</label>
-			<textarea name="description" rows="4" id="description"  class="form-control">{{$task->description}}</textarea>
-			@error('description')
-				<div class="error-message">{{ $message }}</div>
-			@enderror
-		</div>
-		<div class="form-group">
-			<label for="long_description">Long description</label>
-			<textarea name="long_description" rows="7" id="long_description" class="form-control">{{$task->long_description}}</textarea>
-			@error('long_description')
-				<div class="error-message">{{ $message }}</div>
-			@enderror
-		</div>
-		<button type="submit">Edit task</button>
-	</form>
+  @include('component.form', ['task' => $task])
 @endsection
